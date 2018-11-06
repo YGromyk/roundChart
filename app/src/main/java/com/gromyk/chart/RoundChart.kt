55da5e11b38f0dd6270cb050e0431cdad246fbc5
Yurii Gromyk <yurii.gromyk@magnise.com>
2018-11-06 13:04:27 +0200

    Optimized and refactored view.

--- app/src/main/java/com/gromyk/chart/RoundChart.kt
+++ app/src/main/java/com/gromyk/chart/RoundChart.kt
@@ -20,6 +20,10 @@ class RoundChart @JvmOverloads constructor(
 
     companion object {
         private const val START_ANGLE = -90
+        private const val pickerRadius = 25f
+        private const val EXTERNAL_CIRCLE_COEFFICIENT_RADIUS = 1.05f
+        private const val INNER_CIRCLE_COEFFICIENT_RADIUS = 1.25f
+        private const val MAIN_CIRCLE_COEFFICIENT_RADIUS = 2f
 
         @Throws(IllegalArgumentException::class)
         fun getAngleByPercent(percent: Float): Float {
@@ -27,26 +31,96 @@ class RoundChart @JvmOverloads constructor(
                 throw IllegalArgumentException("Value " + percent.toString() + " cannot be used as percent!")
             return (360f / 100f) * percent
         }
+
+        fun createAntiAliasingPaint(): Paint {
+            val paint = Paint()
+            paint.flags = Paint.ANTI_ALIAS_FLAG
+            paint.isAntiAlias = true
+            return paint
+        }
     }
 
     private var viewWidth = 0
     private var viewHeight = 0
-
-    private val externalBorderCirclePath = Path()
-    private val innerBorderCirclePath = Path()
-    private val externalCirclePicker = Path()
-    private val innerCirclePicker = Path()
-    private val mainCircle = Path()
-
-    private lateinit var gradientPicker: LinearGradient
-    private lateinit var gradientCircle: LinearGradient
-
+    private var shadowRadius = 50f
+    private var strokeWidth = 20f
+    private var textSize = 30f
 
     var percentExternal: Float = 0f
     var percentInner: Float = 0f
 
+    private lateinit var externalCirclePaint: Paint
+    private lateinit var innerCirclePaint: Paint
+    private lateinit var externalCirclePickerPaint: Paint
+    private lateinit var innerCirclePickerPaint: Paint
+    private lateinit var mainCirclePaint: Paint
+    private lateinit var labelPaint: Paint
+    private lateinit var restOfExternalCircle: Paint
+    private lateinit var restOfInnerCircle: Paint
+
+    private lateinit var gradientPicker: LinearGradient
+    private lateinit var mainCircleGradient: LinearGradient
+    private lateinit var secondaryCircleGradient: LinearGradient
+
     init {
         init(attrs)
+        initPaints()
+    }
+
+    private fun initPaints() {
+        externalCirclePaint = createAntiAliasingPaint()
+        innerCirclePaint = createAntiAliasingPaint()
+        externalCirclePickerPaint = createAntiAliasingPaint()
+        innerCirclePickerPaint = createAntiAliasingPaint()
+        mainCirclePaint = createAntiAliasingPaint()
+        restOfExternalCircle = createAntiAliasingPaint()
+        restOfInnerCircle = createAntiAliasingPaint()
+        setupLabelPaint()
+        setupCirclePaint(innerCirclePaint, Paint.Style.STROKE)
+        setupCirclePaint(externalCirclePaint, Paint.Style.STROKE)
+        setupRestOfCircle(restOfExternalCircle)
+        setupRestOfCircle(restOfInnerCircle)
+        setupMainCircle(mainCirclePaint)
+    }
+
+
+    private fun setupCirclePaint(paint: Paint, style: Paint.Style) {
+        paint.isAntiAlias = true
+        paint.flags = Paint.ANTI_ALIAS_FLAG
+        paint.color = Color.CYAN
+        paint.style = style
+        paint.strokeWidth = strokeWidth
+    }
+
+    private fun setPaintGradient(paint: Paint, linearGradient: LinearGradient) {
+        paint.setShadowLayer(shadowRadius, 0f, 0f, Color.BLACK)
+        setLayerType(LAYER_TYPE_SOFTWARE, paint)
+        paint.shader = linearGradient
+    }
+
+    private fun setupRestOfCircle(paint: Paint) {
+        paint.color = Color.RED
+        paint.strokeWidth = strokeWidth
+        paint.style = Paint.Style.STROKE
+    }
+
+    private fun setupLabelPaint() {
+        labelPaint = Paint()
+        labelPaint.color = Color.BLACK
+        labelPaint.textSize = textSize
+    }
+
+    fun setTextSize(size: Int) {
+        textSize = dpToPx(size).toFloat()
+    }
+
+    fun setupMainCircle(paint: Paint) {
+        paint.style = Paint.Style.FILL
+        paint.color = Color.WHITE
+    }
+
+    private fun dpToPx(dp: Int): Int {
+        return (dp * resources.displayMetrics.density).toInt()
     }
 
     private fun init(attrs: AttributeSet?) {
@@ -56,70 +130,55 @@ class RoundChart @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-//        drawExternalCircle(canvas)
         drawCircle(
             canvas,
-            getRadius(viewWidth.toFloat(), viewHeight.toFloat()),
-            percentExternal,
-            externalBorderCirclePath,
-            Paint.Style.STROKE,
-            gradientCircle
+            externalCirclePaint,
+            getRadius(viewWidth.toFloat(), viewHeight.toFloat()) / EXTERNAL_CIRCLE_COEFFICIENT_RADIUS,
+            percentExternal
         )
         drawCircle(
             canvas,
-            getRadius(viewWidth.toFloat(), viewHeight.toFloat()) / 1.25f,
-            percentInner,
-            innerBorderCirclePath,
-            Paint.Style.STROKE,
-            gradientCircle
-        )
-
-        drawCircle(
-            canvas,
-            getRadius(viewWidth.toFloat(), viewHeight.toFloat()) / 2f,
-            100f,
-            mainCircle,
-            Paint.Style.FILL,
-            gradientCircle
+            innerCirclePaint,
+            getRadius(viewWidth.toFloat(), viewHeight.toFloat()) / INNER_CIRCLE_COEFFICIENT_RADIUS,
+            percentInner
         )
 
         drawPicker(
             canvas,
-            getRadius(viewWidth.toFloat(), viewHeight.toFloat()),
-            percentExternal,
-            externalCirclePicker,
-            gradientPicker
+            externalCirclePickerPaint,
+            getRadius(viewWidth.toFloat(), viewHeight.toFloat()) / EXTERNAL_CIRCLE_COEFFICIENT_RADIUS,
+            percentExternal
         )
         drawPicker(
             canvas,
-            getRadius(viewWidth.toFloat(), viewHeight.toFloat()) / 1.25f,
-            percentInner,
-            innerCirclePicker,
-            gradientPicker
+            innerCirclePickerPaint,
+            getRadius(viewWidth.toFloat(), viewHeight.toFloat()) / INNER_CIRCLE_COEFFICIENT_RADIUS,
+            percentInner
         )
+        canvas.drawCircle(
+            width / 2f,
+            height / 2f,
+            getRadius(viewWidth.toFloat(), viewHeight.toFloat()) / MAIN_CIRCLE_COEFFICIENT_RADIUS,
+            mainCirclePaint
+        )
+        val textWidth = labelPaint.measureText("This is a text.")
+        canvas.drawText("This is a text.", (viewWidth / 2f) - textWidth / 2, viewHeight / 2f, labelPaint)
+        canvas.drawText("This is a text.", (viewWidth / 2f) - textWidth / 2, (viewHeight / 2f) + textSize, labelPaint)
+
     }
 
     private fun drawCircle(
-        canvas: Canvas, circleRadius: Float, percentAngle: Float,
-        circlePath: Path, style: Paint.Style, linearGradient: LinearGradient) {
+        canvas: Canvas,
+        paint: Paint,
+        circleRadius: Float,
+        percentAngle: Float
+    ) {
         val width = width.toFloat()
         val height = height.toFloat()
-
-        circlePath.reset()
-        circlePath.addCircle(width / 2, height / 2, circleRadius, Path.Direction.CW)
-
-        val paint = Paint()
-        paint.setShadowLayer(50f, 0f, 0f, Color.BLACK)
-        setLayerType(LAYER_TYPE_SOFTWARE, paint)
-        paint.color = Color.CYAN
-        paint.strokeWidth = 20f
-        paint.style = Paint.Style.STROKE
-        paint.shader = linearGradient
 
         val centerX: Float
         val centerY: Float
         val oval = RectF()
-        paint.style = style
 
         centerX = width / 2
         centerY = height / 2
@@ -131,34 +190,18 @@ class RoundChart @JvmOverloads constructor(
             centerY + circleRadius
         )
         canvas.drawArc(oval, START_ANGLE.toFloat(), getAngleByPercent(percentAngle), false, paint)
-        paint.color = Color.RED
-        paint.strokeWidth = 20f
-        paint.style = Paint.Style.STROKE
-        paint.shader = LinearGradient(
-            0f, 0f, 0f, getHeight().toFloat(),
-            Color.RED, Color.WHITE, Shader.TileMode.MIRROR
-        )
-        canvas.drawArc(oval, getAngleByPercent(percentAngle) - 90, 360 - getAngleByPercent(percentAngle), false, paint)
+        if (percentAngle != 100f)
+            canvas.drawArc(
+                oval, getAngleByPercent(percentAngle) - 90,
+                360 - getAngleByPercent(percentAngle), false, restOfInnerCircle
+            )
     }
 
-    private fun drawPicker(canvas: Canvas, circleRadius: Float, percentAngle: Float, pickerPath: Path, linearGradient: LinearGradient) {
+    private fun drawPicker(canvas: Canvas, paint: Paint, circleRadius: Float, percentAngle: Float) {
         val angleRadians = Math.toRadians(getAngleByPercent(percentAngle).toDouble())
         val centerX = viewWidth / 2f + Math.sin(angleRadians).toFloat() * circleRadius
         val centerY = viewHeight / 2f - Math.cos(angleRadians).toFloat() * circleRadius
-
-        pickerPath.reset()
-        pickerPath.addCircle(
-            centerX, centerY,
-            5f, Path.Direction.CW
-        )
-
-        val paint = Paint()
-        paint.strokeWidth = 10f
-        paint.style = Paint.Style.STROKE
-        paint.shader = linearGradient
-        paint.style = Paint.Style.FILL
-        val radius = 25f
-        canvas.drawCircle(centerX, centerY, radius, paint)
+        canvas.drawCircle(centerX, centerY, pickerRadius, paint)
     }
 
     private fun getRadius(viewWidth: Float, viewHeight: Float): Float {
@@ -180,14 +223,24 @@ class RoundChart @JvmOverloads constructor(
     }
 
     private fun initGradients() {
-        gradientCircle = LinearGradient(
+        mainCircleGradient = LinearGradient(
             0f, 0f, 0f, height.toFloat(),
-            Color.GREEN, Color.CYAN, Shader.TileMode.CLAMP
+            Color.GREEN, Color.CYAN, Shader.TileMode.REPEAT
         )
         gradientPicker = LinearGradient(
             0f, 0f, 0f, height.toFloat(),
             Color.GREEN, Color.CYAN, Shader.TileMode.CLAMP
         )
+        secondaryCircleGradient = LinearGradient(
+            0f, 0f, 0f, height.toFloat(),
+            Color.RED, Color.WHITE, Shader.TileMode.MIRROR
+        )
+        setPaintGradient(externalCirclePaint, mainCircleGradient)
+        setPaintGradient(innerCirclePaint, mainCircleGradient)
+        setPaintGradient(innerCirclePickerPaint, gradientPicker)
+        setPaintGradient(externalCirclePickerPaint, gradientPicker)
+        setPaintGradient(restOfExternalCircle, secondaryCircleGradient)
+        setPaintGradient(restOfInnerCircle, secondaryCircleGradient)
     }
 
 }
